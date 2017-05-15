cbd505c05bfb87a87352594eff587474b5ef91a1
evelynhuangyt <evelynhuangyt@163.com>
2017-05-15 19:22:55 -0400

    basic server framework & root page

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var indexRouter = require("./routes/index");
+var mongoose = require("mongoose");
+
+//connect to the database
+mongoose.connect("DATABASE INFO");
+
+//configure the static file root
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/node_modules", express.static(__dirname + "/node_modules"));
+
+//route different urls to different routers
+app.use("/", indexRouter);
+
+app.listen(3000);
